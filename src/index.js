a90ea1139924e1502ba19c7e0a8b14ad91807597
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-02-16 14:47:19 +0100

    Create a convertBinaryToDecimal fnc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./main.scss";
 
 console.log("connected");
+
+let power = 0;
+let decimalSum = 0;
+
+const convertBinaryToDecimal = (num) => {
+
+  let arr = num.split('').map(Number);
+  console.log(arr)
+
+  for (let index = arr.length - 1; index >= 0; index -= 1) {
+    decimalSum += arr[index] * 2 ** power;
+    power += 1;
+  }
+
+  console.log(decimalSum);
+}
+
+convertBinaryToDecimal("1111");
+
